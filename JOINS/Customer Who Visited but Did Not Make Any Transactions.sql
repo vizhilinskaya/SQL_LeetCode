cb5a013dff281fb0b0e5eda00bb2999b17b2933c
Vika <zilinskaavika@gmail.com>
2025-11-19 11:21:05 +0300

    Update Customer Who Visited but Did Not Make Any Transactions.sql

--- JOINS/Customer Who Visited but Did Not Make Any Transactions.sql
+++ JOINS/Customer Who Visited but Did Not Make Any Transactions.sql
@@ -1,3 +1,6 @@
+/*Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+Return the result table sorted in any order.*/
+
 SELECT v.customer_id, 
   COUNT(v.visit_id) AS count_no_trans 
 FROM Visits v
@@ -28,9 +31,6 @@ Table: Transactions
 +----------------+---------+
 transaction_id is column with unique values for this table.
 This table contains information about the transactions made during the visit_id.
-
-Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
-Return the result table sorted in any order.
 
 The result format is in the following example.
 
