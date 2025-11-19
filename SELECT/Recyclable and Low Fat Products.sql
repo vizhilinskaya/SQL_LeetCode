7d25869ee449bab1cfe95dc0e06a98d2a9eacf4b
Vika <zilinskaavika@gmail.com>
2025-11-19 09:44:07 +0300

    Update Recyclable and Low Fat Products.sql

--- SELECT/Recyclable and Low Fat Products.sql
+++ SELECT/Recyclable and Low Fat Products.sql
@@ -24,7 +24,14 @@ Products table:
 
 Write a solution to find the ids of products that are both low fat and recyclable.
 Return the result table in any order.
-The result format is in the following example.*/
+The result format is in the following example.
+Output: 
++-------------+
+| product_id  |
++-------------+
+| 1           |
+| 3           |
++-------------+*/
 
 SELECT product_id 
 FROM Products
