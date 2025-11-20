57363704e847b8a341371f57a477db0b9aeeb2d5
Vika <zilinskaavika@gmail.com>
2025-11-20 12:25:11 +0300

    Create Queries Quality and Percentage.sql

---
+++ Aggregate_Functions/Queries Quality and Percentage.sql
@@ -0,0 +1,47 @@
+/*We define query quality as: The average of the ratio between query rating and its position.
+We also define poor query percentage as: The percentage of all queries with rating less than 3.
+Write a solution to find each query_name, the quality and poor_query_percentage.
+Both quality and poor_query_percentage should be rounded to 2 decimal places.
+Return the result table in any order.*/
+
+SELECT query_name,
+       ROUND(AVG(CASE WHEN position > 0 THEN rating / position ELSE NULL END), 2) AS quality,
+       ROUND((COUNT(CASE WHEN rating < 3 THEN 1 END) / COUNT(*)) * 100, 2) AS poor_query_percentage
+  FROM Queries
+ GROUP BY query_name;
+
+/*Table: Queries
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| query_name  | varchar |
+| result      | varchar |
+| position    | int     |
+| rating      | int     |
++-------------+---------+
+This table may have duplicate rows.
+This table contains information collected from some queries on a database.
+The position column has a value from 1 to 500.
+The rating column has a value from 1 to 5. Query with rating less than 3 is a poor query.
+
+Example 1:
+Input: 
+Queries table:
++------------+-------------------+----------+--------+
+| query_name | result            | position | rating |
++------------+-------------------+----------+--------+
+| Dog        | Golden Retriever  | 1        | 5      |
+| Dog        | German Shepherd   | 2        | 5      |
+| Dog        | Mule              | 200      | 1      |
+| Cat        | Shirazi           | 5        | 2      |
+| Cat        | Siamese           | 3        | 3      |
+| Cat        | Sphynx            | 7        | 4      |
++------------+-------------------+----------+--------+
+Output: 
++------------+---------+-----------------------+
+| query_name | quality | poor_query_percentage |
++------------+---------+-----------------------+
+| Dog        | 2.50    | 33.33                 |
+| Cat        | 0.66    | 33.33                 |
++------------+---------+-----------------------+
+*/
