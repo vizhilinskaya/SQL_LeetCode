2ab584f6d75eb0a9f67486e43d1a4e83ee120caa
Vika <zilinskaavika@gmail.com>
2025-11-20 16:06:37 +0300

    Create Classes With at Least 5 Students.sql

---
+++ Sorting_and_Grouping/Classes With at Least 5 Students.sql
@@ -0,0 +1,41 @@
+/*Write a solution to find all the classes that have at least five students.
+Return the result table in any order.*/
+
+SELECT class
+  FROM Courses
+ GROUP BY class
+HAVING COUNT(student) >= 5;
+
+/*Table: Courses
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+
+Example 1:
+Input: 
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output: 
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+*/
