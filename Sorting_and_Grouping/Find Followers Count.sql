3a33c25d1927dde0dbdd6b536ba67212ce260e8b
Vika <zilinskaavika@gmail.com>
2025-11-20 16:31:19 +0300

    Create Find Followers Count.sql

---
+++ Sorting_and_Grouping/Find Followers Count.sql
@@ -0,0 +1,39 @@
+/*Write a solution that will, for each user, return the number of followers.
+Return the result table ordered by user_id in ascending order.*/
+
+SELECT user_id, 
+       COUNT(follower_id) AS followers_count
+  FROM Followers
+ GROUP BY user_id
+ ORDER BY user_id ASC;
+
+/*Table: Followers
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| user_id     | int  |
+| follower_id | int  |
++-------------+------+
+(user_id, follower_id) is the primary key (combination of columns with unique values) for this table.
+This table contains the IDs of a user and a follower in a social media app where the follower follows the user.
+
+Example 1:
+Input: 
+Followers table:
++---------+-------------+
+| user_id | follower_id |
++---------+-------------+
+| 0       | 1           |
+| 1       | 0           |
+| 2       | 0           |
+| 2       | 1           |
++---------+-------------+
+Output: 
++---------+----------------+
+| user_id | followers_count|
++---------+----------------+
+| 0       | 1              |
+| 1       | 1              |
+| 2       | 2              |
++---------+----------------+
+*/
