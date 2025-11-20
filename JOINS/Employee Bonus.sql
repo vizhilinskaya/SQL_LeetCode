fb1691255f121b17ec8257613862be85267c3f87
Vika <zilinskaavika@gmail.com>
2025-11-20 11:04:41 +0300

    Update Employee Bonus.sql

--- JOINS/Employee Bonus.sql
+++ JOINS/Employee Bonus.sql
@@ -1,6 +1,5 @@
 /*Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
-Return the result table in any order.
-The result format is in the following example.*/
+Return the result table in any order.*/
 
 SELECT name, 
        bonus
