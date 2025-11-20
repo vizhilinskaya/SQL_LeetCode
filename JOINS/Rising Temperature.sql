94069929ec17e872d121eeb686b6ad0ee36d8ef8
Vika <zilinskaavika@gmail.com>
2025-11-20 11:09:16 +0300

    Update Rising Temperature.sql

--- JOINS/Rising Temperature.sql
+++ JOINS/Rising Temperature.sql
@@ -2,10 +2,10 @@
 Return the result table in any order.*/
   
 SELECT w1.id
-FROM Weather w1
-JOIN Weather w2
-  ON w1.recordDate - w2.recordDate = 1
-WHERE w1.temperature > w2.temperature;
+  FROM Weather w1
+       JOIN Weather w2
+       ON w1.recordDate - w2.recordDate = 1
+ WHERE w1.temperature > w2.temperature;
 
 /*Table: Weather
 +---------------+---------+
