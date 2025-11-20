07e6e14b5525d386011a091a30e8d697bdcf34d2
Vika <zilinskaavika@gmail.com>
2025-11-20 11:10:47 +0300

    Update Students and Examinations.sql

--- JOINS/Students and Examinations.sql
+++ JOINS/Students and Examinations.sql
@@ -4,14 +4,14 @@ Return the result table ordered by student_id and subject_name.*/
 SELECT s.student_id,
        s.student_name,
        sub.subject_name,
-COALESCE(COUNT(e.student_id), 0) AS attended_exams
-FROM Students s
-CROSS JOIN Subjects sub
-LEFT JOIN Examinations e 
-  ON s.student_id = e.student_id 
-  AND sub.subject_name = e.subject_name
-GROUP BY s.student_id, s.student_name, sub.subject_name
-ORDER BY s.student_id, sub.subject_name;
+       COALESCE(COUNT(e.student_id), 0) AS attended_exams
+  FROM Students s
+       CROSS JOIN Subjects sub
+       LEFT JOIN Examinations e 
+       ON s.student_id = e.student_id 
+          AND sub.subject_name = e.subject_name
+ GROUP BY s.student_id, s.student_name, sub.subject_name
+ ORDER BY s.student_id, sub.subject_name;
 
 /*Table: Students
 +---------------+---------+
