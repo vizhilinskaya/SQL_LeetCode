8b346c414732497c4b47cf31f8e3cc35ef0e8253
Vika <zilinskaavika@gmail.com>
2025-11-20 12:26:04 +0300

    Update Average Selling Price.sql

--- Aggregate_Functions/Average Selling Price.sql
+++ Aggregate_Functions/Average Selling Price.sql
@@ -1,4 +1,5 @@
-/*Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places. If a product does not have any sold units, its average selling price is assumed to be 0.
+/*Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places. 
+If a product does not have any sold units, its average selling price is assumed to be 0.
 Return the result table in any order.*/
 
 SELECT p.product_id, 
