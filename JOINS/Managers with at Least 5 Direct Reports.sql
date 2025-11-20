f7124bd4dc37f33eba4835fa41698eb058e84317
Vika <zilinskaavika@gmail.com>
2025-11-20 10:54:38 +0300

    Create Managers with at Least 5 Direct Reports.sql

---
+++ JOINS/Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,43 @@
+/*Write a solution to find managers with at least five direct reports.
+Return the result table in any order.*/
+
+SELECT m.name 
+FROM employee m
+JOIN employee e ON e.managerId = m.id
+GROUP BY m.id
+HAVING COUNT(e.id) >= 5;
+
+/*Table: Employee
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+
+Example 1:
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | null      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+
+*/
