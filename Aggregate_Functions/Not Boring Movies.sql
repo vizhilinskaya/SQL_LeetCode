9f54e2a219c085ba73e77234d798f3ce5fc6cff5
Vika <zilinskaavika@gmail.com>
2025-11-20 11:27:28 +0300

    Create Not Boring Movies.sql

---
+++ Aggregate_Functions/Not Boring Movies.sql
@@ -0,0 +1,47 @@
+/*Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+Return the result table ordered by rating in descending order.*/
+
+SELECT id, 
+       movie, 
+       description, 
+       rating
+  FROM Cinema
+ WHERE id % 2 = 1 
+   AND description NOT IN ('boring')
+ ORDER BY rating DESC;
+
+/*Table: Cinema
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| id             | int      |
+| movie          | varchar  |
+| description    | varchar  |
+| rating         | float    |
++----------------+----------+
+id is the primary key (column with unique values) for this table.
+Each row contains information about the name of a movie, its genre, and its rating.
+rating is a 2 decimal places float in the range [0, 10]
+
+Example 1:
+Input: 
+Cinema table:
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 1  | War        | great 3D    | 8.9    |
+| 2  | Science    | fiction     | 8.5    |
+| 3  | irish      | boring      | 6.2    |
+| 4  | Ice song   | Fantacy     | 8.6    |
+| 5  | House card | Interesting | 9.1    |
++----+------------+-------------+--------+
+Output: 
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 5  | House card | Interesting | 9.1    |
+| 1  | War        | great 3D    | 8.9    |
++----+------------+-------------+--------+
+*/
+
+
